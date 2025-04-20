50f69679a969be592dab891125b6e252175b213a
Tereso del Rio <delriot@coventry.ac.uk>
2025-04-20 21:26:11 +0200

    Translating messages.js

--- src/schemas/messages.js
+++ src/schemas/messages.js
@@ -1,89 +1,128 @@
+import i18next from 'i18next';
+
 const messagesObj = {
-    newInheritanceSuccess: {
-            title: 'Herencia creada',
-            text: "La herencia se ha creado correctamente",
-            icon: "success"
-    },
-    newInheritanceError: {
-        title: 'Error',
-        text: "Error creando herencia",
-        icon: "error"
-    },
-    valorationAddedSuccess: {
-        title: 'Valoracion añadida',
-        text: "La Valoracion se ha añadido correctamente",
-        icon: "success"
-    },
-    valorationAddedError: {
-        title: 'Error',
-        text: "Error añadiendo valoracion",
-        icon: "error"
-    },
-    calculateSuccess: {
-        title: 'Calculando',
-        text: "La solución se esta calculando, puede tardar unos minutos...",
-        icon: "success"
-    },
-    calculateError: {
-        title: 'Error',
-        text: "Error iniciando el cálculo, prueba mas tarde",
-        icon: "error"
-    },
-    solutionNotFoundError: {
-        title: 'Error',
-        text: "La solución no ha sido calculada todavía",
-        icon: "error"
-    },
-    emailVerifiedSuccess: {
-        title: 'Email verificado',
-        text: 'Ahora puedes acceder',
-        icon: "success"
-    },
-    UserNotFoundException: {
-        title: 'Email no encontrado',
-        icon: "error"
-    },
-    networkError: {
-        title: "Error de conexión",
-        icon: "error"
-    },
-    sessionError: {
-        title: "La sesión ha expirado",
-        icon: "error"
-    },
-    accessDeniedError: {
-        title: "Accesso denegado",
-        icon: "error"
-    },
-    itemNotFoundError: {
-        title: "Elemento no encontrado",
-        icon: "error"
-    },
-    unexpectedError: {
-        title: "Error insesperado, inténtalo más tarde",
-        icon: "error"
-    },
-    deleteInheritanceConfirmation: {
-        title: "¿Estás seguro?",
-        text: "Una vez eliminada no podrá ser recuperada!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#e32e36',
-        cancelButtonColor: '#1b263b',
-        confirmButtonText: "Eliminar",
-        cancelButtonText: "Cancelar"
-    },
-    deleteInheritanceSuccess: {
-        title: "Herencia eliminada",
-        icon: "success"
-    },
-    passwordUpdated: {
-        title: "Constraseña actualizada",
-        icon: "success"
-    },
-    passwordUpdatedError: {
-        title: "Error actualizando constraseña",
-        icon: "error"
-    },
-}
+  get newInheritanceSuccess() {
+    return {
+      title: i18next.t('messages-newInheritanceSuccess-title'),
+      text: i18next.t('messages-newInheritanceSuccess-text'),
+      icon: "success"
+    };
+  },
+  get newInheritanceError() {
+    return {
+      title: i18next.t('messages-newInheritanceError-title'),
+      text: i18next.t('messages-newInheritanceError-text'),
+      icon: "error"
+    };
+  },
+  get valorationAddedSuccess() {
+    return {
+      title: i18next.t('messages-valorationAddedSuccess-title'),
+      text: i18next.t('messages-valorationAddedSuccess-text'),
+      icon: "success"
+    };
+  },
+  get valorationAddedError() {
+    return {
+      title: i18next.t('messages-valorationAddedError-title'),
+      text: i18next.t('messages-valorationAddedError-text'),
+      icon: "error"
+    };
+  },
+  get calculateSuccess() {
+    return {
+      title: i18next.t('messages-calculateSuccess-title'),
+      text: i18next.t('messages-calculateSuccess-text'),
+      icon: "success"
+    };
+  },
+  get calculateError() {
+    return {
+      title: i18next.t('messages-calculateError-title'),
+      text: i18next.t('messages-calculateError-text'),
+      icon: "error"
+    };
+  },
+  get solutionNotFoundError() {
+    return {
+      title: i18next.t('messages-solutionNotFoundError-title'),
+      text: i18next.t('messages-solutionNotFoundError-text'),
+      icon: "error"
+    };
+  },
+  get emailVerifiedSuccess() {
+    return {
+      title: i18next.t('messages-emailVerifiedSuccess-title'),
+      text: i18next.t('messages-emailVerifiedSuccess-text'),
+      icon: "success"
+    };
+  },
+  get UserNotFoundException() {
+    return {
+      title: i18next.t('messages-UserNotFoundException-title'),
+      icon: "error"
+    };
+  },
+  get networkError() {
+    return {
+      title: i18next.t('messages-networkError-title'),
+      icon: "error"
+    };
+  },
+  get sessionError() {
+    return {
+      title: i18next.t('messages-sessionError-title'),
+      icon: "error"
+    };
+  },
+  get accessDeniedError() {
+    return {
+      title: i18next.t('messages-accessDeniedError-title'),
+      icon: "error"
+    };
+  },
+  get itemNotFoundError() {
+    return {
+      title: i18next.t('messages-itemNotFoundError-title'),
+      icon: "error"
+    };
+  },
+  get unexpectedError() {
+    return {
+      title: i18next.t('messages-unexpectedError-title'),
+      icon: "error"
+    };
+  },
+  get deleteInheritanceConfirmation() {
+    return {
+      title: i18next.t('messages-deleteInheritanceConfirmation-title'),
+      text: i18next.t('messages-deleteInheritanceConfirmation-text'),
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#e32e36',
+      cancelButtonColor: '#1b263b',
+      confirmButtonText: i18next.t('messages-deleteInheritanceConfirmation-confirmButtonText'),
+      cancelButtonText: i18next.t('messages-deleteInheritanceConfirmation-cancelButtonText')
+    };
+  },
+  get deleteInheritanceSuccess() {
+    return {
+      title: i18next.t('messages-deleteInheritanceSuccess-title'),
+      icon: "success"
+    };
+  },
+  get passwordUpdated() {
+    return {
+      title: i18next.t('messages-passwordUpdated-title'),
+      icon: "success"
+    };
+  },
+  get passwordUpdatedError() {
+    return {
+      title: i18next.t('messages-passwordUpdatedError-title'),
+      icon: "error"
+    };
+  }
+};
+
 export default messagesObj;
